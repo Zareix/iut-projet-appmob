5acf2d657c51602b8df59f88e81c9db5d850d5d8
Zareix <raphcatarino@gmail.com>
2021-02-11 11:26:25 +0100

    created navbar using fragments

--- app/src/main/java/fr/iut/appmobprojet/MainActivity.java
+++ app/src/main/java/fr/iut/appmobprojet/MainActivity.java
@@ -1,8 +1,18 @@
 package fr.iut.appmobprojet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import fr.iut.appmobprojet.fragments.AccountFragment;
+import fr.iut.appmobprojet.fragments.AddFragment;
+import fr.iut.appmobprojet.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment homeF = new HomeFragment();
+        Fragment addF = new AddFragment();
+        Fragment accF = new AccountFragment();
+
+        // Lors d'un clic sur la navbar :
+        // On change le fragment par celui correspondant
+        // --> On redirige l'utilisateur sur la bonne page
+        makeCurrentFragment(homeF);
+        BottomNavigationView navbar = findViewById(R.id.bottom_navbar);
+        navbar.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.ic_home:
+                    makeCurrentFragment(homeF);
+                    break;
+                case R.id.ic_add:
+                    makeCurrentFragment(addF);
+                    break;
+                case R.id.ic_account:
+                    makeCurrentFragment(accF);
+                    break;
+            }
+            return true;
+        });
+    }
+
+    /**
+     * Redirige vers le fragment choisi
+     *
+     * @param f
+     */
+    private void makeCurrentFragment(Fragment f){
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.fl_wrapper, f).commit();
     }
 }

0aed71569942b8977f173e906e4ca3914a78d6cb
lolammrn <60889697+lolammrn@users.noreply.github.com>
2021-03-22 02:21:15 +0100

    fragment add complete

--- app/src/main/java/fr/iut/appmobprojet/fragments/AddFragment.java
+++ app/src/main/java/fr/iut/appmobprojet/fragments/AddFragment.java
@@ -1,5 +1,7 @@
 package fr.iut.appmobprojet.fragments;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,7 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,6 +26,10 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,7 +39,11 @@ import fr.iut.appmobprojet.R;
 /**
  * Fragment correspondant à la page d'ajout d'un produit
  */
-public class AddFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+public class AddFragment extends Fragment implements AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
+
+    private static TextView textPeremption;
+    private static String typeNourriture;
+    private Timestamp datePeremptionBdd;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -55,49 +69,89 @@ public class AddFragment extends Fragment implements AdapterView.OnItemSelectedL
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         Spinner spinner = (Spinner) view.findViewById(R.id.addProduct_type);
+        textPeremption = view.findViewById(R.id.textPeremption);
+        Button buttonPeremption = view.findViewById(R.id.buttonPeremption);
+        Button buttonValiderAddProduct = view.findViewById(R.id.buttonValiderAddProduct);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.products_type_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.products_type_array, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(this);
 
+        buttonPeremption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
 
-        Map<String, Object> data = new HashMap<>();
-        data.put("codePostal", "75016");
-        data.put("dateAjout", Timestamp.now());
-        data.put("donneur", "william");
-        data.put("marque", "Panzani");
-        data.put("peremption", new Timestamp(new Date()));
-        data.put("titre", "Spaghettis");
-        data.put("typeNourriture", "Féculents");
+        EditText titre, marque, codePostal;
+
+        titre = view.findViewById(R.id.addProduct_titre);
+        marque = view.findViewById(R.id.addProduct_marque);
+        codePostal = view.findViewById(R.id.addProduct_codePostal);
+
+        buttonValiderAddProduct.setOnClickListener(v -> {
+            Map<String, Object> data = new HashMap<>();
+            data.put("codePostal", codePostal.getText().toString().trim());
+            data.put("dateAjout", Timestamp.now());
+            data.put("donneur", getContext().getSharedPreferences("user", Context.MODE_PRIVATE).getString("username", null));
+            data.put("marque", marque.getText().toString().trim());
+            data.put("peremption", datePeremptionBdd);
+            data.put("titre", titre.getText().toString().trim());
+            data.put("typeNourriture", typeNourriture);
 
 
-        db.collection("products")
-                .add(data)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("NewProductActivity", "DocumentSnapshot written with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("NewProductActivity", "Error adding document", e);
-                    }
-                });
+            db.collection("products")
+                    .add(data)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d("NewProductActivity", "DocumentSnapshot written with ID: " + documentReference.getId());
+                            Toast.makeText(getContext(), "Produit ajouté !", Toast.LENGTH_SHORT).show();
+                            titre.getText().clear();
+                            marque.getText().clear();
+                            codePostal.getText().clear();
+                            textPeremption.setText("Date de péremption");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("NewProductActivity", "Error adding document", e);
+                        }
+                    });
+        });
+
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog dpd = new DatePickerDialog(getContext(), this, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        dpd.show();
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String sSelected = parent.getItemAtPosition(position).toString();
-        Toast.makeText(getContext(), sSelected, Toast.LENGTH_SHORT).show();
+        typeNourriture = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getContext(), typeNourriture, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        month++;
+        String date = dayOfMonth + "/" + month + "/" + year;
+        textPeremption.setText(date);
+        try {
+            datePeremptionBdd= new Timestamp(new SimpleDateFormat("dd/MM/yyyy").parse(dayOfMonth + "/" + month +"/" + year));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
